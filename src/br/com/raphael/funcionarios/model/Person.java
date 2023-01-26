7d5b43f1e4b0971a3473216f522ce59a6edd3334
raphael <raphaelprsilva@gmail.com>
2023-01-26 17:22:04 -0300

    fix: update names to english language

--- src/br/com/raphael/funcionarios/model/Person.java
+++ src/br/com/raphael/funcionarios/model/Person.java
@@ -4,22 +4,22 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public abstract class Person {
-    private String nome;
-    private LocalDate dataNascimento;
+    private String name;
+    private LocalDate birthDate;
 
-    public Person(String nome, LocalDate dataNascimento) {
-        Objects.requireNonNull(nome);
-        Objects.requireNonNull(dataNascimento);
+    public Person(String name, LocalDate birthDate) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(birthDate);
 
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
+        this.name = name;
+        this.birthDate = birthDate;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
+    public LocalDate getBirthDate() {
+        return birthDate;
     }
 }
